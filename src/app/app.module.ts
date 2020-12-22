545baf2b86fc2cf18b2df73ba5e6d561a5e6e6da
AbdurahmanovaTuran <turancha@code.edu.az>
2020-12-22 19:55:27 +0400

    side-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SidemenuComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/sidemenu/sidemenu.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: ['./sidemenu.component.scss'],
+})
+export class SidemenuComponent implements OnInit {
+  persons: any;
+  searchedWorks = '';
+  // profileImgs: Entry<any>[] = [];
+  opened: boolean = false;
+  education: boolean = true;
+  experience: boolean = false;
+  contacts: boolean = false;
+  skills: boolean = false;
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.persons = [
+      'personal',
+      'education',
+      'skills',
+      'experience',
+      'contacts',
+    ];
+
+    // this.contentfulService
+    //   .getProfileImgs()
+    //   .then((profileImgs) => (this.profileImgs = profileImgs));
+
+    this.opened = true;
+  }
+
+  getChosenSpeciality(chosenSpeciality?: string) {
+    this.education = false;
+    this.experience = false;
+    this.contacts = false;
+    this.skills = false;
+    if (chosenSpeciality == 'education') {
+      this.education = true;
+    } else if (chosenSpeciality == 'experience') {
+      this.experience = true;
+    } else if (chosenSpeciality == 'contacts') {
+      this.contacts = true;
+    } else if (chosenSpeciality == 'skills') {
+      this.skills = true;
+    }
+  }
+
+  isHidden() {
+    return this.router.url.includes('search');
+  }
+
+  search(searchForm: NgForm) {
+    this.opened = false;
+    this.router.navigate(['/search', this.searchedWorks, '1']);
+  }
+
+  isActive(activated: string, current: string) {
+    return activated === current;
+  }
+
+  isMenuOpen() {
+    this.opened = !this.opened;
+  }
+}
